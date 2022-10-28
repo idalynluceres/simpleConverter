2f8d2fd05e935c38a446663f79346ff5ecbbae09
yayeen <idalynluceres22@gmail.com>
2022-10-28 18:23:12 +0800

    Added CSS Style

--- js/script.js
+++ js/script.js
@@ -4,8 +4,9 @@ function yard() {
     let inches = document.getElementById("lengthInput").value;
     const yard = 36;
     const ans = inches / yard;
+    const result = ans.toFixed(2);
 
-    document.getElementById("result").innerHTML = ans;
+    document.getElementById("result").innerHTML = result;
     document.getElementById("resultUnit").innerHTML = "Yards";
 }
 
@@ -13,8 +14,9 @@ function meter() {
     let inches = document.getElementById("lengthInput").value;
     const meter = 39.37;
     const ans = inches / meter;
+    const result = ans.toFixed(2);
 
-    document.getElementById("result").innerHTML = ans;
+    document.getElementById("result").innerHTML = result;
     document.getElementById("resultUnit").innerHTML = "Meters";
 }
 
@@ -22,7 +24,8 @@ function feet() {
     let inches = document.getElementById("lengthInput").value;
     const feet = 12;
     const ans = inches / feet;
+    const result = ans.toFixed(2);
 
-    document.getElementById("result").innerHTML = ans;
+    document.getElementById("result").innerHTML = result;
     document.getElementById("resultUnit").innerHTML = "Feet";
 }
